c21a33a3202dbbeeb4738ec4fca4ed6619380f92
Abramov Viacheslav <aslava@mail.ru>
2023-07-12 01:03:26 +0300

    fix complited transactions

--- src/main.py
+++ src/main.py
@@ -3,23 +3,26 @@ import json
 from transactions import Transaction
 from datetime import datetime
 
-datafile = '..\data\operations.zip'
+datafile = '..\data\operations.zip' # адрес архива с данными
 
 def show_last_five(datafile):
-    trans_list = []
+    '''выводит пять последних удачных операций'''
+    trans_list = [] # сюда выгружаем данные
     with ZipFile(datafile) as zip_file:
         with zip_file.open('operations.json') as file:
             data = json.loads(file.read().decode('utf-8'))
-            for item in data:
+            for item in data: # переносим всё в список экземпляров класса Transaction, если данных не достаточно, просто пропускаем запись (это не касается открытия вклада, где есть только счет получатель)
                 try:
                     trans_list.append(Transaction(item['id'], datetime.fromisoformat(item['date']), item['state'], item['operationAmount'], item['description'], item.get('from', None), item['to']))
                 except:
                     continue
 
-    trans_list.sort(key=lambda x: x.date, reverse=True)
+    trans_list.sort(key=lambda x: x.date, reverse=True) # сортируем по дате и времени
 
-    for i in range(5):
-        print(trans_list[i])
+    final_list = filter(lambda x: x.state == 'EXECUTED', trans_list) # итератор на только состоявшиеся транзакции
+
+    for i in range(5):# выводим пять последних операций
+        print(next(final_list))
         print()
 
 
